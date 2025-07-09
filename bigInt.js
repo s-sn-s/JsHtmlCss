927d8fa91790045c2ebfc8ffa684f4c498256e95
s-sn-s <savin.sn.shetty@gmail.com>
2025-07-09 21:39:38 +0530

    jspwLecture20250709

---
+++ bigInt.js
@@ -0,0 +1,9 @@
+let num = 9899897989799999n;
+
+console.log(num);
+
+console.log(Number.MAX_SAFE_INTEGER);
+
+//console.log(num+200);
+//results in error
+console.log(num+200n);
---
+++ switchMyCase.js
@@ -0,0 +1,16 @@
+let browser = 'mine';
+
+console.log('Switch cases!');
+
+switch(browser){
+  case 'yours':
+    console.log(`the answer is ${browser}`);
+    break;
+  case 'ohh':
+    console.log(`the answer is ${browser}`);
+    break;
+  default:
+    console.log(`Sorry! 
+      the answer is ${browser}`);
+    break;
+}
